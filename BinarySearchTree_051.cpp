1a4a44a7d1a4d603c6a22394e1b3bc69e34a7800
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-06-14 13:50:33 +0700

    mendeklarasikan fungsi main

--- BinarySearchTree_051.cpp
+++ BinarySearchTree_051.cpp
@@ -116,4 +116,57 @@ public:
 			cout << ptr->info << "  ";
 		}
 	}
-}; 
+};  
+
+int main()
+{
+	BinaryTree obj;
+	while (true)
+	{
+		cout << "\nMENU" << endl;
+		cout << "1. Implement insert operation" << endl;
+		cout << "2. Perform inorder traversal" << endl;
+		cout << "3. Perform preorder traversal" << endl;
+		cout << "4. Perform postorder traversal" << endl;
+		cout << "5.Exit" << endl;
+		cout << "\nEnter Your Choice(1-5) : ";
+
+		char ch;
+		cin >> ch;
+		cout << endl;
+
+		switch (ch)
+		{
+		case'1': {
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			obj.insert(word);
+			break;
+		}
+		case'2':
+		{
+			obj.inorder(obj.ROOT);
+			break;
+		}
+		case'3':
+		{
+			obj.preorder(obj.ROOT);
+			break;
+		}
+		case'4':
+		{
+			obj.postorder(obj.ROOT);
+			break;
+		}
+		case'5':
+			return 0;
+		default:
+		{
+			cout << "invalid option" << endl;
+			break;
+		}
+		}
+
+	}
+}
