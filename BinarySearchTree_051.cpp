5aa975f792cec80b5d1a64b288927a204167bd9c
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-06-14 13:45:51 +0700

    mendeklarasikan fungsi insert

--- BinarySearchTree_051.cpp
+++ BinarySearchTree_051.cpp
@@ -27,3 +27,61 @@ public:
 	{
 		ROOT = NULL; //initializing Root to null
 	}
+	void insert(string element) // insert a node in the binary search tree
+	{
+		node* NewNode = new node(element, NULL, NULL); // allocate memory for the new node
+		NewNode->info = element; //Assign value to the data field of the new node
+		NewNode->leftchild = NULL; //Make the left child of the new node point to null
+		NewNode->rightchild = NULL; //Make the right child of the new node point to NULL
+
+		node* parent = NULL;
+		node* currentNode = NULL;
+		search(element, parent, currentNode); //Locate the node which will be the parent of the node to be inserted
+
+		if (parent == NULL)// if the parent is NULL (Tree is empty)
+		{
+			ROOT = NewNode;  // mark the new node as ROOT
+			return; //Exit
+		}
+
+
+		if (element < parent->info) // if the value in the data field of the new node is less than that of the parent
+		{
+			parent->leftchild = NewNode; //make the left child of the parent point to the new node
+		}
+
+		else if (element > parent->info) //if the value in the data field of the new node is greater that that of the parent
+		{
+			parent->rightchild = NewNode; // make the right child of the parent point to the new node
+		}
+	}
+
+	void search(string element, node*& parent, node*& currentNode)
+	{
+		// this function searches the current node of the specified node as well as the current node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
+
+	void inorder(node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			inorder(ptr->leftchild);
+			cout << ptr->info << "  ";
+			inorder(ptr->rightchild);
+		}
+	}
